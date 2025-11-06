1a1c9fbc77bd15581b39fba5ad63af014ffe8338
InJRaven <kuuhaku989898@gmail.com>
2025-11-06 14:36:37 +0700

    Delete redundant folders and files

--- src/modules/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TokenService, EntityVerifier } from '@/shared';
-
-@Module({
-  providers: [TokenService, EntityVerifier],
-  exports: [TokenService, EntityVerifier],
-})
-export class SharedModule {}
